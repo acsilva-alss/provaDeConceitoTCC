f93bba902bdc45c106d2f0e012813cdc0ff40bcf
Alisson Chagas Silva <alisson.cs@hotmail.com>
2020-08-26 12:32:32 -0300

    add

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Server } from "./server";
  
 const server = new Server();
- 
+const port = process.env.PORT;
 server.listen(port => {
- console.log(`Server is listening on http://localhost:${process.env.PORT}`);
+ console.log(`Server is listening on http://localhost:${port}`);
 });
