9da7027892597173390b96d39db14325764223e4
Alisson Chagas Silva <alisson.cs@hotmail.com>
2020-08-27 15:08:46 -0300

    add

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Server } from "./server";
 //import path from "path";
 import 'dotenv/config'
 const server = new Server();
-const port = process.env.PORT;
+
 server.listen(port => {
  console.log(`Server is listening on http://localhost:${port}`);
 });
